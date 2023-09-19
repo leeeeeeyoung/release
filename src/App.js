21973f5d235c702aee1c3b89d1736b41a38c3548
goyoung <leey6015@outlook.com>
2023-09-19 15:12:15 +0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { useEffect, useRef, useState, MutableRefObject } from 'react'
+
+function App() {
+
+
+   useEffect(()=>{
+   
+           // 在组件挂载完成后，使用fetch获取数据
+    fetch('https://aigctech.love/web.php/user/info',{
+            method:'GET',
+            headers:{
+               'X-Token':'650595deb79e61',
+            }
+
+      })
+      .then(response => {
+          
+          console.log(response.json())
+          })
+      ;
+
+   },[])
+
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
